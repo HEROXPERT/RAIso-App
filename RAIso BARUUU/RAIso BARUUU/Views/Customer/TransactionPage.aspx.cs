1f1dd900e4e0181b7ed6e63dbe09278d7b4c4255
valen <valen@ASUS>
2024-10-06 20:50:31 +0700

    Add project files.

---
+++ RAIso BARUUU/RAIso BARUUU/Views/Customer/TransactionPage.aspx.cs
@@ -0,0 +1,34 @@
+﻿using RAIso_BARUUU.Model;
+using RAIso_BARUUU.Repository;
+using System;
+using System.Collections.Generic;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RAIso_BARUUU.Views.Customer
+{
+    public partial class TransactionPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["user"] == null)
+            {
+                Response.Redirect("~/Views/Guest/Login.aspx");
+            }
+            else
+            {
+                MsUser currentUser = (MsUser)Session["user"];
+                List<TransactionHeader> th = TransactionHeaderRepository.getTransactionHeaderByUserId(currentUser.UserID);
+                TransactionGV.DataSource = th;
+                TransactionGV.DataBind();
+            }
+        }
+
+        protected void TransactionGV_SelectedIndexChanging(object sender, GridViewSelectEventArgs e)
+        {
+            GridViewRow row = TransactionGV.Rows[e.NewSelectedIndex];
+            int id = Convert.ToInt32(row.Cells[0].Text);
+            Response.Redirect("~/Views/Customer/TransactionDetailPage.aspx?id=" + id);
+        }
+    }
+}
